3b02371c4bfdc7e621771352f7f7160898c3fec2
MUHIMPUNDU Anne <muhimpunduan@gmail.com>
2025-07-31 21:01:13 +0200

    Delete prisma directory

--- prisma/migrations/20250731083022_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "Course" ALTER COLUMN "description" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "Student" ALTER COLUMN "phone" DROP NOT NULL,
-ALTER COLUMN "profilePicture" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "lastName" DROP NOT NULL;
